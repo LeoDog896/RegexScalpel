6040a17331d719b56c0d9322a09b0678379da16a
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-01-26 20:03:27 -0500

    fix: add openjfx to maven conf

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
         // // 1. Interactively fix a single regex
         // SingleTester.Test_Interactively("a*a*b");
         // // 2. Automatically fix a single regex
-        // SingleTester.Test_Automatically("a*a*b");
+         SingleTester.Test_Automatically("a*a*b"  );
         // // 3. Automatically repair regexs in dataset
         // DatasetTester.Test("data/input/CVE415-SOLA-DA.txt", "data/output/CVE415-SOLA-DA.txt");
     }
